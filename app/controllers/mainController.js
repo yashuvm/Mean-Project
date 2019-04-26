901a7fb20929aef9aae3a292c786d0fface4bf4c
sushma <sushmashukla937@gmail.com>
2019-04-26 16:30:09 +0530

    sdfcgvh
    sedfcgvhb

---
+++ app/controllers/mainController.js
@@ -0,0 +1,28 @@
+/**
+ * Created by root on 18/4/19.
+ */
+(function () {
+  var injectParams = ['$scope',  '$rootScope', '$window', '$location','$http','mainService'];
+  var mainController = function ($scope, $rootScope, $window, $location,  $http, mainService) {
+    
+
+    //testing-->
+    $scope.testing = function () {
+      mainService.testing().success(function (results) {
+        if (results) { 
+          ($scope.titile = results) ;
+         
+        }
+        else {
+          // DebugService.logData(result.statusMessage);
+        }
+      });
+    };
+
+    //testing -->
+
+
+  };
+  mainController.$inject = injectParams;
+  angular.module('seniorApp').controller('mainController', mainController);
+}());
