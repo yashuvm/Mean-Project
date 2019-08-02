31910110bcd3a60699fda60c4a6a67d838897193
yashu <yashuvm@gmail.com>
2019-08-02 16:33:46 +0530

    a
    a

---
+++ app/controllers/clusterManagerDetail.js
@@ -0,0 +1,42 @@
+'use strict';
+
+(function () {
+
+  var injectParams = ['$scope','$rootScope','$location','$filter'];
+  var clusterManagerDetailController = function ($scope,$rootScope,$location,$filter) {
+
+
+//     $scope.clicked1 = function(){
+//       alert("Successfully data Send");
+//   }
+  
+  
+  // $scope.customerId = $rootScope.customerIdValue ;
+ 
+  $scope.clustervalue = [{
+    value:"",
+  }];
+  //  $scope.custvalue.value =   $rootScope.customerDetails ; 
+  $scope.clusterIdData = function(){
+    $scope.clusterId = $rootScope.clusterIdValue ;
+    $scope.clustervalue.value =   $rootScope.clusterManagerDetails ;
+
+    angular.forEach($scope.clustervalue.value, function (element) {
+      $scope.clusterValue = element;
+        if( $scope.clusterId == $scope.clusterValue.clm_id){
+          $scope.clustervalue = $scope.clusterValue;
+        }
+       
+      })
+
+  };
+
+  
+  
+ 
+    };
+
+    clusterManagerDetailController.$inject = injectParams;
+    angular.module('seniorApp')
+      .controller('clusterManagerDetailController', clusterManagerDetailController)
+  })();
